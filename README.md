6b4492b4f8b18972234ec0a8c9336a140a0fc940
Veronicandemo <ndemoveronica@gmail.com>
2022-06-28 17:54:13 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,57 @@
+# Belafonte Food Hub
+*****
+### Author : Martin Chege 21st June 2022
+****
+## Project Description
+Using a fetch API ("https://www.themealdb.com/api/json/v1/1/search.php?s=") to get a list of meals I came up with Belafonte Food Hub website that offers users a variety of meals and using a button one can watch how to prepare the meals, like or dislike based on their preference and Submit a review at the button of the page.
+
+Getting:
+1.List of meals/dish.
+2.Name of the meal.
+3.Category: such as Soup or side dish.
+4.Origin of the meal.
+5.Youtube link to learn how to prepare the meal with the aid of a video.
+******
+
+## SCREENSHOTS
+- ![Screenshot from 2022-06-26 21-33-24](https://user-images.githubusercontent.com/24671358/175829051-5c3bd3ed-01ab-4597-8d54-10bd83a9df6e.png)
+- ![Screenshot from 2022-06-26 21-33-34](https://user-images.githubusercontent.com/24671358/175829052-fb18cd3e-bdac-489b-867b-187a79a29d70.png)
+- ![Screenshot from 2022-06-26 21-33-51](https://user-images.githubusercontent.com/24671358/175829055-4038b828-29bf-4d39-bf07-8cd0ee8b2e51.png)
+
+
+********
+## SetUp Instruction
+### Requirements
+* [HTML](lang Url)
+* Text editor eg [Visual Studio Code](https://code.visualstudio.com/download)
+
+
+### Getting Files
+* Fork the repo
+- Create a new branch in your terminal (git checkout -b improve-feature)
+- Install the prerequisites.
+- Make appropriate changes in file(s)
+- Run the server to see the changes
+- Add the changes and commit them (git commit -am "Improve App")
+- Push to the branch (git push origin improve-app)
+- Create a Pull request
+* Open the folder location on terminal and use the following command to run app:
+
+## How To Run It
+Use live share extension to run the HTML file.
+## Live Link
+Or you can access the web application directly via this [LINK.](link.com/)
+*****
+
+## Technologies Used
+1. HTML
+2. CSS
+3. JavaScript
+
+*****
+## Contact Information
+* Email : martin.chege@student.moringaschool.com
+*****
+## [License](LICENSE)
+MIT License
+Copyright (c) 2022 Martin Chege
---
+++ index.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const url = "www.themealdb.com/api/json/v1/1/search.php?f=a";
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    // To get each item loop through the array
+    console.log(data);
+    let html = "";
+    data.meals.map((value) => {
+      html += `
+            <div class="col-md-4">
+
+            <div class="card" style="width:370px">
+            <h2 id="tee">${value.strArea}</h2>
+            <p>${value.strTags}</p>
+                <img class="card-img-top" src="${value.strMealThumb}" alt="Card image" style="width:100%">
+                <div class="card-body">
+                    <h4 class="card-title text-success">${value.strMeal}</h4>
+                <div class="major">
+                    <div><a href="${value.strYoutube}" class="btn btn-outline-success">Watch Video</a></div>
+
+                    </div>
+                    
+                </div>
+                <div class="all"> 
+                    
+                <div class="lke"> 
+                    <button id="likebtn">
+                    <i class="fa fa-thumbs-up"></i>
+                    </button>
+                     <input type="number" id="input1" value="0" name=""></input>
+                </div>
+
+                <div class="dis">
+                <button id="dislikebtn">
+                    <i class="fa fa-thumbs-down"></i>
+                </button>
+                <input type="number" id="input2" value="0" name=""></input>
+                    </div>
+                    </div>       
+                    </div>
+            </div>
+            
+        </div>`;
+    });
+    document.getElementById("content").innerHTML = html;
+
+    //Like button
+    let likebtn = document.querySelector("#likebtn");
+    //Dislike buttons
+    let dislikebtn = document.querySelector("#dislikebtn");
+    let input1 = document.querySelector("#input1");
+    let input2 = document.querySelector("#input2");
+
+    //Event Listener:click
+    likebtn.addEventListener("click", () => {
+      input1.value = parseInt(input1.value) + 1;
+      input1.style.color = "#12ff00";
+    });
+    //Event Listener:click
+    dislikebtn.addEventListener("click", () => {
+      input2.value = parseInt(input2.value) + 1;
+      input2.style.color = "#ff0000";
+    });
+    //DOMContentLoaded
+    document.addEventListener("DOMContentLoaded", () => {
+      html();
+    });
+    //Review form
+    const mealReviews = document.getElementById("review-points");
+    //Event Listener:Submit
+    document.querySelector("#form").addEventListener("submit", (e) => {
+      e.preventDefault();
+      let text = document.querySelector("#form textarea");
+      let review = document.createElement("li");
+      review.innerText = text.value;
+      mealReviews.appendChild(review);
+      document.querySelector("#form").reset();
+    });
+  })
+  .catch((err) => console.log(err));
